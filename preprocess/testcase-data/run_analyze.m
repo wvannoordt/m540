d76fda6480916e8cba2bbd8df7c25f7161781c71
W. van Noordt <47154608+wvannoordt@users.noreply.github.com>
2019-04-12 13:54:17 -0600

    Add files via upload

---
+++ preprocess/testcase-data/run_analyze.m
@@ -0,0 +1,20 @@
+function [numout, meansout, stdevsout] = run_analyze(filename)
+  data = csvread(filename);
+
+[n1, n2] = size(data);
+data_length = ceil(n1/2);
+evens = 2*(1:data_length);
+odds = evens - 1;
+numout = data(evens,1);
+
+loadtimes = [];
+for i = 1:length(odds)
+  loadtimes(i, :) = data(odds(i), 2:end);
+end
+computetimes = [];
+for i = 1:length(evens)
+  computetimes(i, :) = data(evens(i), 2:end);
+end
+meansout = mean(computetimes')';
+stdevsout = std(computetimes')';
+end
